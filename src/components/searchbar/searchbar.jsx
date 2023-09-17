5c33e9da9d6d2c32abd0fa07cce96026bb764ea8
Святослав Минчаков <sminchakov@gmail.com>
2023-09-18 00:28:25 +0300

    coments deleted

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -46,39 +46,3 @@ export class Searchbar extends Component {
     );
   }
 }
-
-// export class Searchbar extends Component {
-//   state = {
-//     value: '',
-//   };
-
-//   handleChange = e => {
-//     this.setState({ value: e.target.value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.getFormValue(this.state.value);
-//     this.setState({ value: '' });
-//   };
-
-//   render() {
-//     return (
-//       <FormWrapper>
-//         <form onSubmit={this.handleSubmit}>
-//           <input
-//             value={this.state.value}
-//             onChange={this.handleChange}
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//           />
-//           <button>
-//             <span>Search</span>
-//           </button>
-//         </form>
-//       </FormWrapper>
-//     );
-//   }
-// }
